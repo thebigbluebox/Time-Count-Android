f5b6cfd80c7f4f2b6089cf5ad9d6eadcdf447b92
s7aesis <;>
2014-11-01 17:40:46 -0400

    Made Post Asynchronous

--- app/src/main/java/com/datify/counter/myapplication/MainActivity.java
+++ app/src/main/java/com/datify/counter/myapplication/MainActivity.java
@@ -2,13 +2,32 @@ package com.datify.counter.myapplication;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ListActivity {
@@ -23,6 +42,62 @@ public class MainActivity extends ListActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 R.layout.rowlayout, R.id.label, values);
         setListAdapter(adapter);
+
+        new MyHttpPost().execute();
+    }
+
+    //DELETE THIS -- TEMPORARY JSON TEST
+    private class MyHttpPost extends AsyncTask<String, Void, Boolean>
+    {
+        //Log.d("Console", currentDate.toString());
+        @Override
+        protected Boolean doInBackground(String... arg0) {
+            Log.d("Console", "Post entered");/*
+        int TIMEOUT_MILLISEC = 10000;  // = 10 seconds
+        HttpParams httpParams = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(httpParams, TIMEOUT_MILLISEC);
+        HttpConnectionParams.setSoTimeout(httpParams, TIMEOUT_MILLISEC);
+        HttpClient client = new DefaultHttpClient(httpParams);
+        Log.d("Console", "Before request");
+        HttpPost request = new HttpPost("172.26.9.205:9393/time");
+        request.setEntity(new ByteArrayEntity(
+                currentDate.toString().getBytes("UTF8")));
+        HttpResponse response = client.execute(request);*/
+
+            HttpParams params = new BasicHttpParams();
+            Log.d("Console", "HttpParams OK");
+            params.setParameter(CoreProtocolPNames.PROTOCOL_VERSION,
+                    HttpVersion.HTTP_1_1);
+            Log.d("Console", "Param setup OK");
+
+            HttpClient httpclient = new DefaultHttpClient(params);
+            Log.d("Console", "DefaultHttpClient OK");
+            HttpPost httppost = new HttpPost("172.26.9.205:9393/time");
+            Log.d("Console", "HttpPost OK");
+
+            try {
+                // Add your data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                nameValuePairs.add(new BasicNameValuePair("start_date", "12345"));
+                nameValuePairs.add(new BasicNameValuePair("end_date", "AndDev is Cool!"));
+                nameValuePairs.add(new BasicNameValuePair("collection_id", "f4rgtrh5"));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "utf-8"));
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                Log.d("Console", "ClientProtocolException");
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                Log.d("Console", "IOException");
+            }
+
+
+            Log.d("Console", "Post exited");
+            return true;
+        }
     }
 
     @Override
